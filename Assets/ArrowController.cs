b8511afcccc26f4938f4994f5b15bff8eb4408ee
ch3nj <benjaminchen64@gmail.com>
2020-04-20 15:27:29 -0400

    no spam, fixed previous bugs

--- Assets/ArrowController.cs
+++ Assets/ArrowController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
+    public float MAX_X = 10f;
+    public float MAX_Y = 10f;
+    public float MIN_X = -10f;
+    public float MIN_Y = -10f;
+
     public Vector3 arrow_velocity;
     public float gravity = 0.0f;
 
@@ -26,13 +31,15 @@ public class ArrowController : MonoBehaviour
 
     void Move()
     {
-      print(arrow_velocity.ToString());
-      print(gravity);
       Vector3 pos = transform.position;
       arrow_velocity.y -= gravity*0.0001f;
       transform.rotation = Quaternion.LookRotation(Vector3.forward, new Vector3(-arrow_velocity.y, arrow_velocity.x, 0));
       pos += arrow_velocity;
       transform.position = pos;
+      if (pos.x > MAX_X || pos.x < MIN_X || pos.y > MAX_Y || pos.y < MIN_Y) {
+
+        Destroy(this.gameObject);
+      }
 
       Invoke("Move", .01f);
     }
--- Assets/ShootArrow.cs
+++ Assets/ShootArrow.cs
@@ -5,36 +5,48 @@ using UnityEngine;
 public class ShootArrow : MonoBehaviour
 {
     public GameObject arrow;
+    private float timer = 0.0f;
+    private float cooldown = 2.0f;
     private float stringCoeff = 0.05f;
     public float angle = 0.0f;
     public float drawDistance = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-
+      timer = cooldown;
+      angle = 0.0f;
+      drawDistance = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+      timer += Time.deltaTime;
     }
 
     public void changeDraw(float newDraw) {
+      print(newDraw);
       drawDistance = newDraw;
     }
 
     public void changeAngle(float newAngle) {
+      print(newAngle);
       angle = newAngle;
       transform.rotation = Quaternion.LookRotation(Vector3.forward, Quaternion.Euler(0, 0, angle+90) * Vector3.right);
     }
 
     public void Shoot()
     {
+      if (timer < cooldown) {
+        return;
+      }
+      timer = 0.0f;
       print("shooting");
+      print("draw " + drawDistance.ToString());
+      print("angle" + angle.ToString());
       Vector3 pos = transform.position;
-      Instantiate(arrow, pos, Quaternion.identity);
-      ArrowController controller = arrow.GetComponent<ArrowController>();
+      GameObject shot = Instantiate(arrow, pos, Quaternion.identity);
+      ArrowController controller = shot.GetComponent<ArrowController>();
       controller.arrow_velocity = Quaternion.Euler(0, 0, angle) * (stringCoeff*drawDistance*drawDistance*Vector3.right);
     }
 }
